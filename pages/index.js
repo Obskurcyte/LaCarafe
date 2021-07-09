92b2b7c2b7e64fc633ebe672d8c89f12d2ef9127
Obskurcyte <hadrien.jaubert@centrale-marseille>
2021-07-09 11:54:21 +0200

    wola

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 import Button from "../components/Button";
 import Footer from "../components/Footer";
 import Dialog from '@material-ui/core/Dialog';
